c220c7d58bb9e1b1195882d29a6cffd36ce1c718
ayaz-alijamali <ayazalijamali64@gmail.com>
2024-03-28 15:14:44 +0500

    simple helloworld

---
+++ helloworld.js
@@ -0,0 +1,7 @@
+console.log(3 + 2);
+console.log("Ayaz Ali Jamali");
+var userName = "Sagar";
+console.log(userName);
+var num1 = 2;
+var num2 = 3;
+console.log(num1 + num2);
---
+++ helloworld.ts
@@ -0,0 +1,7 @@
+console.log(3+2)
+console.log("Ayaz Ali Jamali")
+let userName="Sagar"
+console.log(userName)
+let num1=2
+let num2=3
+console.log(num1 + num2)
